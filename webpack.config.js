f43b46d7bdc457a05ab3bdddfbd5419426550b66
Dmitry Mazurok <mazurok@outlook.com>
2017-03-20 12:23:26 +0700

    added font-awesome and icons

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: ['./fe/index.js'],
+    entry: ['font-awesome-webpack', './fe/index.js'],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'fe', 'dist')
@@ -18,5 +18,14 @@ module.exports = {
             $: "jquery",
             jQuery: "jquery"
         })
-    ]
+    ],
+    module: {
+        loaders: [
+            // the url-loader uses DataUrls. 
+            // the file-loader emits files. 
+            // for font-awesome
+            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+        ]
+    }
 };
