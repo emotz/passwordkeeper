37b9da034e467d7c588d45d3d972c267a17caf13
Dmitry Mazurok <mazurok@outlook.com>
2017-03-20 10:14:26 +0700

    added async store (emulated), updated splash screen, added animation

--- fe/index.js
+++ fe/index.js
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import dumb from 'bootstrap'; // TODO remove dumb
 import VueRouter from 'vue-router';
 
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
 Vue.use(VueRouter);
 
 const PassAdder = {
@@ -32,16 +36,28 @@ const PassList = {
     components: { 'pass-adder': PassAdder },
     data: function () {
         return {
-            items: [{ title: "hello", user: "user", password: "pass123", show_password: false }]
+            items: [{ title: "hello", user: "user", password: "pass123", show_password: false, stored: "stored" }]
         };
     },
     methods: {
         add: function (item) {
             item.show_password = false;
+            item.stored = "notstored";
             this.items.push(item);
+            this.storeAsync(item);
         },
         remove: function (index) {
             this.items.splice(index, 1);
+        },
+        storeAsync: function (item) {
+            item.stored = "storing";
+            setTimeout(function () {
+                if (getRandomArbitrary(0, 100) > 50) {
+                    item.stored = "stored";
+                } else {
+                    item.stored = "notstored";
+                }
+            }, 3000);
         }
     }
 };
@@ -64,8 +80,7 @@ const app = new Vue({
     el: '#app',
     template: '#app-template',
     data: {
-        project_name: "Password Keeper",
-        loaded: false
+        project_name: "Password Keeper"
     },
     components: {
         'app-nav': {
@@ -73,4 +88,8 @@ const app = new Vue({
         }
     },
     router
+});
+
+$(function () {
+    $("#splash").fadeOut(500);
 });
