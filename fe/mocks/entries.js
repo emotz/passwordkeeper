6a5d8debb153737832a7854c5dd1661b5daee667
Dmitry Mazurok <mazurok@outlook.com>
2017-03-20 21:34:06 +0700

    added server mock

---
+++ fe/mocks/entries.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const entries = require('./entries.json');
+
+/* responses for /users */
+const mockResponses = [
+    /* Respond with 400 Bad Request for PUT and DELETE - inappropriate on a collection */
+    { request: { method: 'PUT' }, response: { status: 400 } },
+    { request: { method: 'DELETE' }, response: { status: 400 } },
+    {
+        /* for GET requests return a subset of data */
+        request: { method: 'GET' },
+        response: function (ctx) {
+            ctx.body = entries;
+        }
+    },
+    {
+        /* for POST requests, create a new entry and return the path to the new resource */
+        request: { method: 'POST', type: 'application/json' },
+        response: function (ctx) {
+            const newEntry = ctx.request.body;
+            if (newEntry.id !== undefined) {
+                ctx.status = 400;
+                ctx.body = { reason: "request must not specify id" };
+                return;
+            }
+            if (newEntry.title === undefined || newEntry.title === "") {
+                ctx.status = 400;
+                ctx.body = { reason: "request must specify non-empty title" };
+                return;
+            }
+            if (newEntry.user === undefined || newEntry.user === "") {
+                ctx.status = 400;
+                ctx.body = { reason: "request must specify non-empty user" };
+                return;
+            }
+
+            entries.push(newEntry);
+            newEntry.id = entries.length;
+            ctx.status = 201;
+            ctx.response.set('Location', `/entries/${newEntry.id}`);
+        }
+    }
+];
+
+module.exports = mockResponses;
---
+++ fe/mocks/entry.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const entries = require('./entries.json');
+const _ = require('lodash');
+
+/* responses for /entries/:id */
+const mockResponses = [
+    /* don't support POST here */
+    { request: { method: 'POST' }, response: { status: 400 } },
+
+    /* for GET requests, return a particular entry */
+    {
+        request: { method: 'GET' },
+        response: function (ctx, id) {
+            ctx.body = entries.find(entry => entry.id === Number(id));
+        }
+    },
+
+    /* for PUT requests, update the record */
+    {
+        request: { method: 'PUT' },
+        response: function (ctx, id) {
+            id = Number(id);
+            const updatedEntry = ctx.request.body;
+            updatedEntry.id = id;
+
+            if (updatedEntry.title === undefined || updatedEntry.title === "") {
+                ctx.status = 400;
+                ctx.body = { reason: "request must specify non-empty title" };
+                return;
+            }
+            if (updatedEntry.user === undefined || updatedEntry.user === "") {
+                ctx.status = 400;
+                ctx.body = { reason: "request must specify non-empty user" };
+                return;
+            }
+
+            const existingEntryIndex = entries.findIndex(entry => entry.id === id);
+            if (existingEntryIndex <= 0) {
+                ctx.status = 404;
+                ctx.body = { reason: "requested id wasn't found" };
+                return;
+            }
+
+            entries.splice(existingEntryIndex, 1, updatedEntry);
+            ctx.status = 200;
+        }
+    },
+
+    /* DELETE request: remove the record */
+    {
+        request: { method: 'DELETE' },
+        response: function (ctx, id) {
+            const existingEntryIndex = entries.findIndex(entry => entry.id === Number(id));
+            entries.splice(existingEntryIndex, 1);
+            ctx.status = 200;
+        }
+    }
+];
+
+module.exports = mockResponses;
