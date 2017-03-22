cece9ae48ea99f248db452480ff6f7079a8fca4b
Dmitry Mazurok <mazurok@outlook.com>
2017-03-22 14:08:03 +0700

    added unit test for checking router links

--- fe/tests/app-nav_test.js
+++ fe/tests/app-nav_test.js
@@ -2,15 +2,40 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import AppNav from 'src/app-nav.vue';
+import router from 'src/router.js';
 
 describe("app-nav tests", function () {
-    Vue.use(VueRouter);
+    function is_there_route_for_location(location) {
+        return router.getMatchedComponents(location).length > 0;
+    }
 
-    it("should mount without errors", function () {
+    beforeEach(function () {
+        jasmine.addMatchers({
+            toBeRegisteredInRouter: function () {
+                return {
+                    compare: function (location) {
+                        var result = { pass: is_there_route_for_location(location) };
+                        if (result.pass) {
+                            result.message = `Expected location ${location} not to be registered in router`;
+                        } else {
+                            result.message = `Expected location ${location} to be registered in router`;
+                        }
+                        return result;
+                    }
+                };
+            }
+        });
+    });
+
+    it("should not contain non-registered links", function () {
         const Component = Vue.extend(AppNav);
 
-        const component = new Component({
-            router: new VueRouter()
-        }).$mount();
+        const component = new Component({ router }).$mount();
+
+        component.$children.forEach(function (child) {
+            const location = child._props.to;
+            if (location === undefined) return;
+            expect(location).toBeRegisteredInRouter();
+        });
     });
 });
