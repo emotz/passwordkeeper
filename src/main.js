eaca7e71d1cb5212ade67cf51f38b57e9f643241
MaHeak <maheak.demons@gmail.com>
2017-03-28 18:48:16 +0700

    test node.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-console.log("Nothing here yet!");
+//console.log("Nothing here yet!");
+var express         = require('express');
+var app = express();
+
+app.listen(1337, function(){
+    console.log('Express server listening on port 1337');
+});
