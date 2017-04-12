920f6485609109c503249a451ac37d1b4e912b34
MaHeak <maheak.demons@gmail.com>
2017-04-12 16:36:30 +0700

    example

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 //console.log("Nothing here yet!");
-var express         = require('express');
+const entries = require('./entries.json');
+
+var express = require('express');
+var path = require('path'); // модуль для парсинга пути
 var app = express();
+
+
+app.get('/api', function (req, res) {
+    res.send('API is running');
+});
+
+app.post('/', function (req, res) {
+  res.send('Got a POST request');
+});
+
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user');
+});
+
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user');
+});
 
 app.listen(1337, function(){
     console.log('Express server listening on port 1337');
