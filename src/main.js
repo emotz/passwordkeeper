6717e14b9a2ea43745e83a51fefb9464718cf004
MaHeak <maheak.demons@gmail.com>
2017-04-12 19:30:59 +0700

    express begining

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 //console.log("Nothing here yet!");
-var express         = require('express');
-var app = express();
+const express = require('express');
+const path = require('path');
+const app = express();
+const entries = require('./entries.json');
+
+app.get('/', function (req, res) {
+    res.send(entries);
+});
+
+app.post('/', function (req, res) {
+  res.send('Got a POST request');
+});
+
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user');
+});
+
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user');
+});
 
 app.listen(1337, function(){
     console.log('Express server listening on port 1337');
