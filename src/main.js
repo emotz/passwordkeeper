26cf06c596e60de0e94e4bc49b65a26a2af52277
MaHeak <maheak.demons@gmail.com>
2017-04-21 16:42:51 +0700

    Added mongoose.js, User and Password schemas. Added log.js for logging. Backend changed for express.

--- src/main.js
+++ src/main.js
@@ -2,24 +2,103 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
 const entries = require('./entries.json');
+const log = require('./libs/log.js')(module);
+
+function guid() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+}
 
 app.use(express.static('fe/dist'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/entries', function (req, res) {
-    res.send(entries);
+app.get('/api/entries', function (req, res, next) {
+    res.json(entries).send();
 });
 
-app.post('/', function (req, res) {
-  res.send('Got a POST request');
+app.get('/api/entries/:id', function (req, res, next) {
+    res.json(enentries.find(entry => entry.id === res.params[0])).send();
 });
 
-app.put('/user', function (req, res) {
-  res.send('Got a PUT request at /user');
+app.post('/api/entries', function (req, res, next) {
+    console.log(req.body);
+    const newEntry = req.body;
+    if (newEntry.id !== undefined) {
+        res.status = 400;
+        res.body = { reason: "request must not specify id" };
+        return;
+    }
+    if (newEntry.title === undefined || newEntry.title === "") {
+        res.status = 400;
+        res.body = { reason: "request must specify non-empty title" };
+        return;
+    }
+    if (newEntry.user === undefined || newEntry.user === "") {
+        ctx.status = 400;
+        ctx.body = { reason: "request must specify non-empty user" };
+        return;
+    }
+
+    entries.push(newEntry);
+    newEntry.id = guid();
+    res.status = 201;
+    res.location(`/api/entries/${newEntry.id}`);
+    res.send();
 });
 
-app.delete('/user', function (req, res) {
-  res.send('Got a DELETE request at /user');
+app.put('/api/entries/:id', function (req, res) {
+    const updatedEntry = req.body;
+    updatedEntry.id = req.params[0];
+
+    if (updatedEntry.title === undefined || updatedEntry.title === "") {
+        res.status = 400;
+        res.body = { reason: "request must specify non-empty title" };
+        return;
+    }
+    if (updatedEntry.user === undefined || updatedEntry.user === "") {
+        res.status = 400;
+        res.body = { reason: "request must specify non-empty user" };
+        return;
+    }
+
+    const existingEntryIndex = entries.findIndex(entry => entry.id === req.params[0]);
+    if (existingEntryIndex < 0) {
+        res.status = 404;
+        res.body = { reason: "requested id wasn't found" };
+        return;
+    }
+    entries.splice(existingEntryIndex, 1, updatedEntry);
+    res.status = 200;
+    res.send('OK');
+});
+
+app.delete('/api/entries/:id', function (req, res) {
+    const existingEntryIndex = entries.findIndex(entry => entry.id === req.params[0]);
+    if (~existingEntryIndex) entries.splice(existingEntryIndex, 1);
+    res.status = 204;
+    res.send('OK');
+});
+
+app.use(function(req, res, next){
+    res.status(404);
+    log.debug('Not found URL: %s',req.url);
+    res.send({ error: 'Not found' });
+    return;
+});
+
+app.use(function(err, req, res, next){
+    res.status(err.status || 500);
+    log.error('Internal error(%d): %s',res.statusCode,err.message);
+    res.send({ error: err.message });
+    return;
 });
 
 app.listen(1337, function(){
