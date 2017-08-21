8a3f0223c4a6e70f4453f1eded3943783d5e6ba3
Dmitry Mazurok <mazurok@outlook.com>
2017-08-21 16:51:08 +0700

    converted from Promise-way to async-way

--- frontend/src/components/pk-config.js
+++ frontend/src/components/pk-config.js
@@ -18,53 +18,27 @@ export default {
         }
     },
     methods: {
-        exportToCSV() {
-            //try {
-                let response = http.get('api/export/csv',
-                    {},
-                    {responseType: 'arraybuffer'}
-                ).then(function (response) {
-                    var headers = response.headers;
-                    var blob = new Blob([response.data],{type:response.headers.get('content-type')});
-                    var link = document.createElement('a');
-                    var filename = response.headers.get('content-disposition').split('filename=')[1].replace('-', '');
-                    console.log(filename);
-                    link.href = window.URL.createObjectURL(blob);
-                    link.download = filename;
-                    return link.click();
-                });
-                //var contentDisposition = response.headers.get('Content-Disposition') || '';
-                //var filename = contentDisposition.split('filename=')[1];
-                //console.log(response.headers.get('content-disposition').split('filename=')[1]);
-                /*let blob = new Blob([response.blob()], {type: response.headers.get('content-type')}),
-                    filename = (response.headers.get('content-disposition') || '').split('filename=')[1];*/
-                /*filename = (response.headers.get('content-disposition') || '').split('filename=')[1];
-                let result = document.createElement('a');
-                result.href = window.URL.createObjectURL(blob);
-                result.download = filename;
-                return result;*/
-            /*}
-            catch (err) {
-                console.log(err);
-                return;
-            }*/
-        },
-        exportToXML() {
-        //try {
-            let response = http.get('api/export/xml',
+        async exportToCSV() {
+            const response = await http.get('api/export/csv',
                 {},
-                {responseType: 'arraybuffer'}
-            ).then(function (response) {
-                console.log(response);
-                var headers = response.headers;
-                var blob = new Blob([response.data],{type:response.headers.get('content-type')});
-                var link = document.createElement('a');
-                var filename = response.headers.get('content-disposition').split('filename=')[1].replace('-', '');
-                console.log(filename);
-                link.href = window.URL.createObjectURL(blob);
-                link.download = filename;
-                return link.click();
-            });
+                { responseType: 'arraybuffer' });
+            const blob = new Blob([response.data], { type: response.headers.get('content-type') });
+            const link = document.createElement('a');
+            const filename = response.headers.get('content-disposition').split('filename=')[1].replace('-', '');
+            link.href = window.URL.createObjectURL(blob);
+            link.download = filename;
+            return link.click();
+        },
+        async exportToXML() {
+            const response = await http.get('api/export/xml',
+                {},
+                { responseType: 'arraybuffer' });
+            const blob = new Blob([response.data], { type: response.headers.get('content-type') });
+            const link = document.createElement('a');
+            const filename = response.headers.get('content-disposition').split('filename=')[1].replace('-', '');
+            link.href = window.URL.createObjectURL(blob);
+            link.download = filename;
+            return link.click();
         }
-    },
+    }
 };
