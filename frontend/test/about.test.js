a2a70679dbf11e21aa0b0ec827f3e08c91fd0329
Dmitry Mazurok <mazurok@outlook.com>
2019-11-23 15:13:11 +0700

    fix tests

--- frontend/test/about.test.js
+++ frontend/test/about.test.js
@@ -1,9 +1,11 @@
-import { Vue } from './vue-helper.js';
 import PkAbout from 'src/components/pk-about.vue';
+import router from 'src/plugins/router.js';
+import { Vue } from './vue-helper.js';
 
 describe("about tests", function() {
   it("should contain greeting message", function() {
-    const component = new Vue(PkAbout).$mount();
+    const Component = Vue.extend(PkAbout);
+    const component = new Component({ router }).$mount();
 
     expect(component.$el.innerHTML.length > 10).toBe(true);
   });
